5e808fc9617062ed8d0e9f3abbf5122b0fd05bd9
dan-lovelight <50263658+dan-lovelight@users.noreply.github.com>
2019-05-04 21:19:26 +1000

    First commit

---
+++ dataUtils.js
@@ -0,0 +1,98 @@
+// This file should always contain the most up to date versions of these functions
+// Copy from here to other files if updates are reqeuired
+
+// Required general utilities
+// > updateLog
+
+const mySecureKnackHeaders = {
+  'Authorization': Knack.getUserToken(),
+  'X-Knack-Application-Id': Knack.application_id,
+  'X-Knack-REST-API-KEY': 'knack',
+  'Content-Type': 'application/json'
+}
+
+// Returns an array of records
+async function filterViewPromise(scene, view, filter) {
+
+  const url = 'https://api.knack.com/v1/pages/scene_' + scene + '/views/view_' + view + '/records/'
+  const search = '?rows_per_page=1000&filters=' + encodeURI(JSON.stringify(filter))
+  const searchUrl = url + search
+  const init = {
+    method: 'GET',
+    headers: mySecureKnackHeaders
+  }
+
+  try {
+    let response = await fetch(searchUrl, init)
+    if (!response.ok) throw Error(response.statusText)
+    let json = await response.json()
+    let records = await json.records
+    return records
+  } catch (err) {
+    logError(filterViewPromise, arguments, err, Knack.getUserAttributes(), window.location.href, true)
+  }
+}
+
+// Updates record and returns the updated record
+async function updateViewPromise(scene, view, id, data) {
+
+  const url = 'https://api.knack.com/v1/pages/scene_' + scene + '/views/view_' + view + '/records/' + id
+  const init = {
+    method: 'PUT',
+    headers: mySecureKnackHeaders,
+    body: JSON.stringify(data)
+  }
+
+  try {
+    let response = await fetch(url, init)
+    if (!response.ok) throw Error(response.statusText)
+    let json = await response.json()
+    return json
+  } catch (err) {
+  logError(updateViewPromise, arguments, err, Knack.getUserAttributes(), window.location.href, true)
+  }
+
+}
+
+// takes an object name (eg 'object_1') and a data object
+// POSTs the data to create the record and returns the record
+async function createViewPromise(scene, view, data) {
+
+  const url = 'https://api.knack.com/v1/pages/scene_' + scene + '/views/view_' + view + '/records/'
+  const init = {
+    method: 'POST',
+    headers: myKnackHeaders,
+    body: JSON.stringify(data)
+  }
+
+  try {
+    let response = await fetch(url, init)
+    if (!response.ok) throw Error(response.statusText)
+    let json = await response.json()
+    return json
+  } catch (err) {
+    logError(createViewPromise, arguments, err, Knack.getUserAttributes(), window.location.href, true)
+  }
+
+}
+
+
+// Returns a single record from a view by ID
+async function getViewRecordPromise(scene, view, id) {
+
+  const url = 'https://api.knack.com/v1/pages/scene_' + scene + '/views/view_' + view + '/records/' + id
+  const init = {
+    method: 'GET',
+    headers: mySecureKnackHeaders
+  }
+
+  try {
+    let response = await fetch(url, init)
+    if (!response.ok) throw Error(response.statusText)
+    let json = await response.json()
+    return json
+  } catch (err) {
+    logError(getViewRecordPromise, arguments, err, Knack.getUserAttributes(), window.location.href, true)
+  }
+
+}
---
+++ generalUtils.js
@@ -0,0 +1,63 @@
+// This file should always contain the most up to date versions of these functions
+// Copy from here to other files if updates are reqeuired
+
+// Takes an error object, and a boolean that indicates if the error should be thrown again
+// logError(*callingFunction*, arguments, err, Knack.getUserAttributes(), window.location.href, true)
+async function logError(callerFunction, args, err, user, url, throwAgain) {
+
+  let callerArgs = Array.prototype.slice.call(args) // Convert caller's arguements to an array
+  let callerArgsNames = getParamNames(callerFunction) // Extract arguement variable names from function code
+  let callerName = err.stack.split('\n')[1].trim().split(' ')[1].trim() // Get the name of the calling function from the Error stack
+
+  // Build an error log entry, in this case for Slack
+  let logMessage = `Error in *${callerName} (` + callerArgsNames.toString().replace(/,/g, ', ') + `)*:exclamation:\n`
+  logMessage += `> *user*: ${user.name} (${user.email})\n`
+  logMessage += `> *url*: ${url} \n`
+  for (var i = 0; i < callerArgs.length; ++i) {
+    logMessage += `> *${callerArgsNames[i]}*: ` + JSON.stringify(callerArgs[i]) + '\n'
+  }
+  logMessage += '```' + err.stack + '```'
+  await updateLog(logMessage) // Send message to Slack
+  if (throwAgain) throw err // Optionally propogate the error
+}
+
+// Used by logError. Takes a function and returns the names of it's parameters
+function getParamNames(func) {
+  const STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+  const ARGUMENT_NAMES = /([^\s,]+)/g;
+  var fnStr = func.toString().replace(STRIP_COMMENTS, '');
+  var result = fnStr.slice(fnStr.indexOf('(') + 1, fnStr.indexOf(')')).match(ARGUMENT_NAMES);
+  if (result === null)
+    result = [];
+  return result;
+}
+
+// Add an entry to the log
+function updateLog(entry) {
+  let url = 'https://hooks.zapier.com/hooks/catch/2107870/jdigp6/'
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({
+      log: entry
+    })
+  })
+}
+
+// Send webhook to Zapier
+async function triggerZap(endPoint, dataObject, logEntry) {
+
+  const zapierAccount = '2107870/'
+  const rootURL = 'https://hooks.zapier.com/hooks/catch/'
+  const url = rootURL + zapierAccount + endPoint
+  const init = {
+    method: 'POST',
+    body: JSON.stringify(dataObject)
+  }
+
+  try {
+    await fetch(url, init)
+    await updateLog(':heavy_check_mark: ' + logEntry)
+  } catch (err) {
+    logError(triggerZap, arguments, err, Knack.getUserAttributes(), window.location.href, true)
+  }
+}
